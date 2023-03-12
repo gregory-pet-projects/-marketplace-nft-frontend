34e24ae3386f67bb22c7682d9623ea45d3c6f835
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-12 17:22:24 +0800

    added links to dicover comp

--- components/NavBar/Discover/Discover.jsx
+++ components/NavBar/Discover/Discover.jsx
@@ -1,7 +1,50 @@
 import Style from "./Discover.module.css";
+import Link from "next/link";
 
+const discover = [
+  {
+    name: "Collection",
+    link: "collection",
+  },
+  {
+    name: "Search",
+    link: "searchPage",
+  },
+  {
+    name: "Author Profile",
+    link: "author",
+  },
+  {
+    name: "NFT Details",
+    link: "NFT-details",
+  },
+  {
+    name: "Account Setting",
+    link: "account",
+  },
+  {
+    name: "Upload NFT",
+    link: "uploadNFT",
+  },
+  {
+    name: "Connect Wallet",
+    link: "connectWallet",
+  },
+  {
+    name: "Blog",
+    link: "blog",
+  },
+];
 const Discover = () => {
-  return <div></div>;
+  return (
+    <div>
+      {discover.map((el, i) => (
+        <div key={i + 1} className={Style.discover}>
+          <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Discover;
