c33626b21b8ddc524a7658eac9aa9e88eb9997aa
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-12 17:29:53 +0800

    added help center items

--- components/NavBar/HelpCenter/HelpCenter.jsx
+++ components/NavBar/HelpCenter/HelpCenter.jsx
@@ -1,7 +1,38 @@
 import Style from "./HelpCenter.module.css";
+import Link from "next/link";
 
+const helpCenter = [
+  {
+    name: "About",
+    link: "aboutus",
+  },
+  {
+    name: "Contact Us",
+    link: "contactus",
+  },
+  {
+    name: "Sign Up",
+    link: "signUp",
+  },
+  {
+    name: "LogIn",
+    link: "login",
+  },
+  {
+    name: "Subscription",
+    link: "subscription",
+  },
+];
 const HelpCenter = () => {
-  return <div></div>;
+  return (
+    <div className={Style.box}>
+      {helpCenter.map((el, i) => (
+        <div className={Style.helpCenter} key={i + 1}>
+          <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default HelpCenter;
