e74179153f6868a515501b3fa66d29c93979511c
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-12 16:22:15 +0800

    added icon import to Navbar

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,8 +1,13 @@
 import Style from "./NavBar.module.css";
-
+import { useState } from "react";
+import { MdNotifications } from "react-icons/md";
+import { BsSearch } from "react-icons/bs";
+import { CgMenuLeft, CgMenuRight } from "react-icons/cg";
 import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index";
 
 const NavBar = () => {
+  const [] = useState();
+  useState(() => {}, []);
   return <div></div>;
 };
 
