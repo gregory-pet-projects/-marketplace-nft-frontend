d37879ba45a7c652ee9fa660a0d2d149e488b83d
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-12 16:29:58 +0800

    added images

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import { MdNotifications } from "react-icons/md";
 import { BsSearch } from "react-icons/bs";
 import { CgMenuLeft, CgMenuRight } from "react-icons/cg";
 import { Discover, HelpCenter, Notification, Profile, SideBar } from "./index";
+import { Button } from "../componentsIndex";
 
 const NavBar = () => {
   const [] = useState();
