a735cb63ff83f062b97cfa06839f9cf48c88b21a
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-22 00:14:24 +0800

    added Hero section

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -1,4 +1,25 @@
+import Image from "next/image";
+import Style from "./HeroSection.module.css";
+import { Button } from "../componentsIndex";
+import images from "../../img";
+
 const HeroSection = () => {
-  return <>Hero</>;
+  return (
+    <div className={Style.heroSection}>
+      <div className={Style.heroSection_box}>
+        <div className={Style.heroSection_box_left}>
+          <h1>Discover, collect and sell NFTs</h1>
+          <p>
+            Discover the most outstanding NTFs in all topics of life. Creative
+            your NTFs and sell them
+          </p>
+          <Button btnName={"Start your search"} />
+        </div>
+        <div className={Style.heroSection_box_right}>
+          <Image src={images.hero} alt="Hero section" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HeroSection;
