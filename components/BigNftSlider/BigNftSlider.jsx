4eb79394e651484dfcb9d71f7d11c75853735775
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-22 11:45:31 +0800

    separated main file for several child components

--- components/BigNftSlider/BigNftSlider.jsx
+++ components/BigNftSlider/BigNftSlider.jsx
@@ -1,14 +1,15 @@
 import Style from "./BigNFTSlider.module.css";
-import { AiFillFire, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { MdVerified, MdTimer } from "react-icons/md";
-import { TbArrowBigLeftLines } from "react-icons/tb";
 import images from "../../img";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import Bidding from "./components/Bidding/Bidding";
+import SliderButtons from "./components/SliderButtons/SliderButtons";
+import MainInfo from "./components/MainInfo/MainInfo";
+import PreviewSection from "./components/PreviewSection/PreviewSection";
 const sliderData = [
   {
     title: "Hello NFT",
     id: 1,
-    name: "Daulat Hussain",
+    name: "Ann Marie",
     collection: "GYm",
     price: "00664 ETH",
     like: 243,
@@ -24,7 +25,7 @@ const sliderData = [
   {
     title: "Buddy NFT",
     id: 2,
-    name: "Shoaib Hussain",
+    name: "Alex Smith",
     collection: "Home",
     price: "0000004 ETH",
     like: 243,
@@ -40,7 +41,7 @@ const sliderData = [
   {
     title: "Gym NFT",
     id: 3,
-    name: "Raayan Hussain",
+    name: "Gregory Petrov",
     collection: "GYm",
     price: "0000064 ETH",
     like: 243,
@@ -56,7 +57,7 @@ const sliderData = [
   {
     title: "Home NFT",
     id: 4,
-    name: "Raayan Hussain",
+    name: "Mikle John",
     collection: "GYm",
     price: "4664 ETH",
     like: 243,
@@ -72,12 +73,48 @@ const sliderData = [
 ];
 const BigNFTSlider = () => {
   const [idNumber, setIdNumber] = useState(1);
+
+  const increment = useCallback(() => {
+    if (idNumber + 1 < sliderData.length) {
+      setIdNumber(idNumber + 1);
+    }
+  }, [idNumber, sliderData.length]);
+
+  const decrement = useCallback(() => {
+    if (idNumber > 0) {
+      setIdNumber(idNumber - 1);
+    }
+  }, [idNumber]);
+
+  const currentSlide = sliderData[idNumber];
+
+  const timerItems = [
+    { time: currentSlide.time.days, title: "Days" },
+    { time: currentSlide.time.hours, title: "Hours" },
+    { time: currentSlide.time.minutes, title: "Minutes" },
+    { time: currentSlide.time.seconds, title: "Seconds" },
+  ];
   return (
     <div className={Style.bigNFTSlider}>
       <div className={Style.bigNFTSlider_box}>
-        <div className={Style.bigNFTSlider_box_left}></div>
-        <div className={Style.bigNFTSlider_box_right}></div>
+        <MainInfo
+          img={currentSlide.image}
+          creatorName={currentSlide.name}
+          collection={currentSlide.collection}
+          title={currentSlide.title}
+        />
+
+        <Bidding
+          timerItems={timerItems}
+          sliderPriceETH={currentSlide.price}
+          sliderPriceUSD={"221,23"}
+          handleClickPlace={() => undefined}
+          handleClickView={() => undefined}
+        />
+
+        <SliderButtons decrement={decrement} increment={increment} />
       </div>
+      <PreviewSection img={currentSlide.nftImage} like={currentSlide.like} />
     </div>
   );
 };
