67b9da6aaa91b381bed2b3a01795712cfe107fa5
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-23 19:36:02 +0800

    added component LikeProfile

---
+++ components/LikeProfile/LikeProfile.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import Style from "./LikeProfile.module.css";
+import images from "../../img";
+
+const LikeProfile = () => {
+  const imageArray = [images.user1, images.user2, images.user3, images.user4];
+  return (
+    <div className={Style.like}>
+      {imageArray.map((item, idx) => (
+        <div className={Style.like_box} key={idx + 1}>
+          <Image
+            src={item}
+            width={15}
+            height={15}
+            key={idx}
+            className={Style.like_box_img}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LikeProfile;
